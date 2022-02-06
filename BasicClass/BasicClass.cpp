7958816fbba8d19492e892dec15553add77fd2b5
Giuseppe DiFlavio <82164747+Geppetto0313@users.noreply.github.com>
2022-02-06 11:43:35 -0400

    Commit 2
    Called more constructors to display that a class is a blueprint of an object.

--- BasicClass/BasicClass.cpp
+++ BasicClass/BasicClass.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <math.h>
 
-
+//Class.
 class Circle {
 private: 
+    //Data Members.
     int Radius;
 public:
+    //Member Functions.
     Circle(int);
     ~Circle();
     int Area();
@@ -13,35 +15,61 @@ public:
     void SetRadius(int);
 };
 
+//Constructor.
 Circle::Circle(int r) {
     Radius = r;
 }
 
+//Deconstructor.
 Circle::~Circle() {
-    std::cout << "Destructor is Called" << std::endl;
+    std::cout << "Destructor(s) is Called" << std::endl;
 }
 
+//Facilitator.
 int Circle::Area() {
     return 3.14 * Radius * Radius;
 }
 
+//Accessor.
 int Circle::GetRadius() {
     return Radius;
 }
 
+//Mutator.
 void Circle::SetRadius(int r) {
     Radius = r;
 }
 
 int main()
 {
-    Circle C(10);
-    std::cout << "Area: " << C.Area() << std::endl;
-    std::cout << "Radius: " << C.GetRadius() << std::endl;
-    std::cout << "Changing Radius of Circle to 20" << std::endl;
-    C.SetRadius(20);
-    std::cout << "Area: " << C.Area() << std::endl;
-    std::cout << "Radius: " << C.GetRadius() << std::endl;
+    //Calling constructor for 3 Circles of Varius sizes.
+    Circle C1(10), C2(22), C3(5);
+
+    //Circle 1.
+    std::cout << "Circle 1:" << std::endl;
+    std::cout << "Area: " << C1.Area() << std::endl;
+    std::cout << "Radius: " << C1.GetRadius() << std::endl;
+    std::cout << std::endl;
+
+    //Circle 2.
+    std::cout << "Circle 2:" << std::endl;
+    std::cout << "Area: " << C2.Area() << std::endl;
+    std::cout << "Radius: " << C2.GetRadius() << std::endl;
+    std::cout << std::endl;
+
+    //Circle 3.
+    std::cout << "Circle 3:" << std::endl;
+    std::cout << "Area: " << C3.Area() << std::endl;
+    std::cout << "Radius: " << C3.GetRadius() << std::endl;
+    std::cout << std::endl;
+
+    //Changing Circle 1 from Radius 10 to 20.
+    std::cout << "Changing Radius of Circle 1 to 20" << std::endl;
+    C1.SetRadius(20);
+    std::cout << "New Circle 1:" << std::endl;
+    std::cout << "Area: " << C1.Area() << std::endl;
+    std::cout << "Radius: " << C1.GetRadius() << std::endl;
+    std::cout << std::endl;
 
     return 0;
 }
